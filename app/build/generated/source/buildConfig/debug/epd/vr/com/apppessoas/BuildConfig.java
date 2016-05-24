e9dceeecbec85482b1f52e74da3b7780688c172c
Carlos Alexandre <carlos.alexandre.smo@gmail.com>
2016-05-24 13:29:02 -0300

    Add apk

---
+++ app/build/generated/source/buildConfig/debug/epd/vr/com/apppessoas/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package epd.vr.com.apppessoas;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "epd.vr.com.apppessoas";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
